d450b70eb04fbd6233f3b830c50005b8b5b5142c
Dominik Cebula <dominikcebula@gmail.com>
2024-11-08 21:09:42 +0100

    aws-samples-lambda-python-cloudwatch-rds

--- db.tf
+++ db.tf
@@ -1,11 +1,9 @@
-resource "aws_db_instance" "default" {
-  allocated_storage           = 1
-  db_name                     = "database01"
+resource "aws_rds_cluster" "aurora_postgresql" {
+  cluster_identifier          = "aurora-cluster-01"
   engine                      = "aurora-postgresql"
-  engine_version              = "15.4"
-  instance_class              = "db.t3.medium"
-  multi_az                    = false
-  username                    = "postgres"
+  availability_zones = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  database_name               = "database01"
+  master_username             = "postgres"
   manage_master_user_password = true
-  skip_final_snapshot         = true
+  backup_retention_period     = 5
 }
