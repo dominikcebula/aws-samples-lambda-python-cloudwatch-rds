1cff6f62ba3d383dd8296cdf80151118f0a9429c
Dominik Cebula <dominikcebula@gmail.com>
2024-11-09 00:18:10 +0100

    aws-samples-lambda-python-cloudwatch-rds

--- db.tf
+++ db.tf
@@ -46,6 +46,12 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
+resource "aws_route" "route" {
+  route_table_id         = aws_vpc.db_vpc.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
 resource "aws_subnet" "db_subnet_a" {
   vpc_id            = aws_vpc.db_vpc.id
   cidr_block        = "10.0.1.0/24"
