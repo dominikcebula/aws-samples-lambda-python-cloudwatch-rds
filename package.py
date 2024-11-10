1a6860e5e031b166882d738d7497bc53fbf44a58
Dominik Cebula <dominikcebula@gmail.com>
2024-11-10 15:32:41 +0100

    aws-samples-lambda-python-cloudwatch-rds

--- package.py
+++ package.py
@@ -5,20 +5,25 @@ from os.path import abspath, exists
 from shutil import rmtree, copytree, make_archive
 from subprocess import run
 
-import venv
-
 ARCHIVE_NAME = "aws-samples-lambda-python-cloudwatch-rds.zip"
 CODE_FOLDER = "code"
 
 
 def main():
+    cleanup()
+
+    build_package()
+
+
+def cleanup():
     remove(ARCHIVE_NAME) if exists(ARCHIVE_NAME) else None
     rmtree("package", ignore_errors=True)
     rmtree("venv", ignore_errors=True)
 
-    venv.create("venv", with_pip=True)
+
+def build_package():
     copytree(CODE_FOLDER, "package")
-    run(["venv/bin/pip", "install", "-r", abspath("package/requirements.txt"), "-t", abspath("package")], cwd=".")
+    run(["pip", "install", "-r", abspath("package/requirements.txt"), "-t", abspath("package")])
     make_archive('aws-samples-lambda-python-cloudwatch-rds.zip', 'zip', 'package')
 
 
