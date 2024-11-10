e5303b1d6ff5f2294a1347a61bb3eb51fa425260
Dominik Cebula <dominikcebula@gmail.com>
2024-11-10 16:31:23 +0100

    aws-samples-lambda-python-cloudwatch-rds

--- code/lambda.py
+++ code/lambda.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 import os
 from pathlib import Path
@@ -20,21 +21,19 @@ aws_region = os.environ['AWS_REGION']
 def lambda_handler(event, context):
     logger.info("Running lambda handler...")
 
-    logger.info("Fetching auth token...")
-    token = get_auth_token()
-    logger.info("Auth token fetched.")
+    token = get_db_auth_token()
+    cloudwatch_client = get_cloudwatch_client()
 
-    logger.info("Opening connection to DB...")
-    with get_connection(token) as connection:
-        logger.info("Connection opened.")
-
-        logger.info("Running sql queries")
-        produce_metrics_for_sql_queries(connection)
+    try:
+        with get_db_connection(token) as connection:
+            produce_metrics_for_sql_queries(connection, cloudwatch_client)
+    finally:
+        cloudwatch_client.close()
 
     logger.info("Lambda handler finished.")
 
 
-def get_auth_token():
+def get_db_auth_token():
     client = boto3.client('rds')
     token = client.generate_db_auth_token(
         DBHostname=endpoint_host_name,
@@ -44,7 +43,11 @@ def get_auth_token():
     return token
 
 
-def get_connection(token):
+def get_cloudwatch_client():
+    return boto3.client('cloudwatch')
+
+
+def get_db_connection(token):
     conn = psycopg2.connect(host=endpoint_host_name,
                             port=port,
                             database=db_name,
@@ -54,22 +57,40 @@ def get_connection(token):
     return conn
 
 
-def produce_metrics_for_sql_queries(connection):
-    produce_metrics_for_sql_query("orders", "sql/orders.sql", connection)
-    produce_metrics_for_sql_query("payments", "sql/payments.sql", connection)
-    produce_metrics_for_sql_query("products", "sql/products.sql", connection)
+def produce_metrics_for_sql_queries(connection, cloudwatch_client):
+    produce_metrics_for_sql_query("orders", "sql/orders.sql", connection, cloudwatch_client)
+    produce_metrics_for_sql_query("payments", "sql/payments.sql", connection, cloudwatch_client)
+    produce_metrics_for_sql_query("products", "sql/products.sql", connection, cloudwatch_client)
 
 
-def produce_metrics_for_sql_query(metric_name, sql_file, connection):
-    logger.info(f"Producing metric {metric_name}...")
+def produce_metrics_for_sql_query(metric_name, sql_file, connection, cloudwatch_client):
     sql_content = Path(sql_file).read_text()
 
     with connection.cursor() as cursor:
+        logger.info(f"Executing query for metrics {metric_name}")
         cursor.execute(sql_content)
         results = cursor.fetchall()
-        print(results)
+        logger.info(f"Query results for {sql_file}: {results}")
+
+        logger.info(f"Sending metrics for metric {metric_name}")
+        put_metrics(cloudwatch_client, metric_name, results)
 
     logger.info(f"Metric {metric_name} produced.")
+
+
+def put_metrics(cloudwatch_client, metric_name, results):
+    cloudwatch_client_response = cloudwatch_client.put_metric_data(
+        Namespace='DB Metrics',
+        MetricData=[
+            {
+                'MetricName': metric_name,
+                'Timestamp': datetime.datetime.now(),
+                'Unit': 'None',
+                'Value': results[0][0]
+            },
+        ]
+    )
+    logger.info(f"Cloudwatch client response for {metric_name}: {cloudwatch_client_response}")
 
 
 if __name__ == "__main__":
